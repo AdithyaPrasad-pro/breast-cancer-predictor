31a4bdcdf0f8e24c1ff301a8d284c7c9ea8c6ba5
AdithyaPrasad-pro <adithya.insaaf@gmail.com>
2026-06-08 18:41:27 +0530

    Experiment with KNN hyperparameters 2

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ y=df["target"]
 
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
 
-model = KNeighborsClassifier(n_neighbors=3)
+model = KNeighborsClassifier(n_neighbors=7)
 model.fit(x_train,y_train)
 
 y_pred= model.predict(x_test)
